5d2b7d802a53c55205335e373b65d9b1ddef736c
Fraser Tooth <frasertooth@gmail.com>
2020-02-06 14:19:04 +0900

    Added Axios and Github API Scrape

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,15 +4,20 @@ import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 function Calculator() {
   const githubAPI = "https://api.github.com/repos/git/git";
 
+  //Example URL Needed  https://github.com/FraserTooth/alwyn
+
   const [url, setName] = useState("");
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
 
+    const size = await axios(githubAPI).data.size;
+    console.log(size);
     alert(`Github URL ${url}`);
   };
 
